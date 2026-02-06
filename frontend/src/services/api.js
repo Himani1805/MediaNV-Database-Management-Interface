28dc89283889033f4d99921f1200a8dfd4f74f48
Himani <140531392+Himani1805@users.noreply.github.com>
2026-02-06 10:59:18 +0000

    update backend api in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://medianv-database-management-interface.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
     'x-api-key': import.meta.env.VITE_API_KEY
